c24020c63a113819c15be5adda7517ef35a5eb8b
takegawalogical <66237056+takegawalogical@users.noreply.github.com>
2020-10-19 17:50:26 +0900

    modify base.php
    modify base.php

--- app/classes/controller/base.php
+++ app/classes/controller/base.php
@@ -10,7 +10,17 @@
  */
 class Controller_Base extends Controller_Template
 {
-    
+
+    protected $data = array();
+
+    protected $header_link = true;
+
+    public function before() {
+
+        if (!empty($this->template) and is_string($this->template)) {
+            // Load the template
+            $this->template = \View::forge($this->template);
+        }
 
         return parent::before();
     }
